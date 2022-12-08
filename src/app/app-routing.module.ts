61684ff5757a4becfda926e6b6e3f880bf73679d
Ftejero21 <fernandotejero566@gmail.com>
2022-12-08 18:29:01 +0100

    añadidoProyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './Components/contact/contact.component';
+import { MainComponent } from './Components/main/main.component';
+import { NotfoundComponent } from './Components/notfound/notfound.component';
+import { PeliculaComponent } from './Components/pelicula/pelicula.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '',  component: MainComponent},
+  {path:'contact' ,component:ContactComponent},
+  {path:'pelicula/:id',component:PeliculaComponent},
+  {path:'**',component:NotfoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { FooterComponent } from './Components/footer/footer.component';
+import { MainComponent } from './Components/main/main.component';
+import{HttpClientModule} from '@angular/common/http';
+import { ContactComponent } from './Components/contact/contact.component';
+import { PeliculaComponent } from './Components/pelicula/pelicula.component';
+import { NotfoundComponent } from './Components/notfound/notfound.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
+    FooterComponent,
+    MainComponent,
+    ContactComponent,
+    PeliculaComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
